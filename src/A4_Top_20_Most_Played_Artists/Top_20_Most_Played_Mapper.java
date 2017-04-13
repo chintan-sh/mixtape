201f2757bff6fe19887ab38963fda2f895e1d651
chintan <chintan.shah104@gmail.com>
2017-04-12 23:20:59 -0400

    Top N pattern

---
+++ src/A4_Top_20_Most_Played_Artists/Top_20_Most_Played_Mapper.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A4_Top_20_Most_Played_Artists;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Chintan
+ */
+public class Top_20_Most_Played_Mapper extends Mapper<Object, Text, Text, IntWritable> {
+    private Text artistName;
+    private IntWritable playCount;
+
+    // incoming input : user-mboxsha1 \t musicbrainz-artist-id \t artist-name \t plays
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String[] artistInfo = value.toString().split("\t");
+
+
+        String aName = artistInfo[2];
+
+        // extract artist name
+        artistName = new Text(aName);
+        playCount = new IntWritable(Integer.parseInt(artistInfo[3].trim()));
+
+        context.write(artistName, playCount);
+    }
+}
