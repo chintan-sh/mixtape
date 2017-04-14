caa713f812e3b09ef2455ae7108e4a1bbdb83a66
chintan <chintan.shah104@gmail.com>
2017-04-13 22:22:18 -0400

    Updated driver params

--- src/A6_Top_20_MoPo_Artists_By_Year/Top_20_Most_Popular_Artist_Driver.java
+++ src/A6_Top_20_MoPo_Artists_By_Year/Top_20_Most_Popular_Artist_Driver.java
@@ -3,39 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package A2_Sum_Play_Count_Per_Artist;
+package A5_Top_20_Most_Popular_Artists;
 
-import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import java.io.IOException;
+
 /**
  *
  * @author Chintan
  */
-public class Artist_Play_Count_Driver {
+public class Top_20_Most_Popular_Artist_Driver {
 
     /**
      * @param args the command line arguments
      */
 
-    public static void main(String[] args) throws 
-            IOException, InterruptedException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Total play count for each Artist");
-        job.setJarByClass(Artist_Play_Count_Driver.class);
-        job.setMapperClass(Artist_Play_Count_Mapper.class);
-        job.setCombinerClass(Artist_Play_Count_Reducer.class);
-        job.setReducerClass(Artist_Play_Count_Reducer.class);
-        job.setOutputKeyClass(Text.class);
+        Job job = Job.getInstance(conf, "Top 20 most popular artist ");
+        job.setJarByClass(Top_20_Most_Popular_Artist_Driver.class);
+
+        job.setMapperClass(Top_20_Most_Popular_Artist_Mapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setNumReduceTasks(1);
+        job.setReducerClass(Top_20_Most_Popular_Artist_Reducer.class);
+        job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(IntWritable.class);
+
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
